645fc588b050471f690be48530eea8c48c2633c6
Esperanza Johnson <MEsperanza.Johnson@uclm.es>
2025-01-26 12:15:16 +0100

    Switch case added for random spawn
    A switch case was added for random spawning. Not quite working as desired though

--- Assets/Scripts/DreamClassScript.cs
+++ Assets/Scripts/DreamClassScript.cs
@@ -10,7 +10,7 @@ public class DreamClassScript : MonoBehaviour
     private bool isNightmare = false;
     private float dreamTimer = 5.0f;
     private int dreamPoint = 0;
-    //public int totalPoints = 0;
+    public int totalPoints = 0;
     private int dreamType;
     private string dreamText = "Test Dream";
     private int attackDreamType = -1;
@@ -19,7 +19,7 @@ public class DreamClassScript : MonoBehaviour
     public DragAndDrop dadItem;
     public GameObject GOItem;
     public spawnDreams DreamSpawner;
-    public TotalPoints totalPoints;
+    public TotalPoints globalPoints;
 
     public enum DreamType
     {
@@ -115,11 +115,14 @@ public class DreamClassScript : MonoBehaviour
             Destroy(GODream);
             DreamSpawner.currentlySpawned--;
             dreamPoint = -1;
-            totalPoints.totalPoints += dreamPoint;
+            totalPoints += dreamPoint;
+            Debug.Log("Total Points Local");
+            Debug.Log(totalPoints.ToString());
+            globalPoints.totalPoints+= dreamPoint;
             Debug.Log("The Dream Type is not the correct one to destroy -1 points");
             Debug.Log(dreamPoint.ToString());
             Debug.Log("Total Dream Points: ");
-            Debug.Log(totalPoints.totalPoints.ToString());
+            Debug.Log(globalPoints.totalPoints.ToString());
         }
 
 
@@ -141,7 +144,10 @@ public class DreamClassScript : MonoBehaviour
         {
             GOItem = collision.gameObject;
             dreamPoint = 1;
-            totalPoints.totalPoints += dreamPoint;
+            globalPoints.totalPoints += dreamPoint;
+            totalPoints += dreamPoint;
+            Debug.Log("Total Points Local");
+            Debug.Log(totalPoints.ToString());
             Destroy(GODream);
             DreamSpawner.currentlySpawned--;
             Debug.Log("Correct Dream Type attack! You get 1 points!");
